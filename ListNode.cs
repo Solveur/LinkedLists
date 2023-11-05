d96ed1cc75bce0033722714a64cff7618203b382
Solveur <bsluch@gmail.com>
2023-11-05 13:07:19 +0200

    Linked list class class

--- ListNode.cs
+++ ListNode.cs
@@ -1,11 +1,39 @@
 ﻿namespace LinkedLists
 {
+	using System.Collections.Generic;
+
 	internal partial class Program
 	{
 		class ListNode
 		{
-			public ListNode Next { get; set; }
-			public int Value { get; set; }
+			public ListNode? Next { get; set; }
+			public int? Value { get; set; }
+
+			public ListNode()	{	}
+
+			public ListNode(IEnumerable<int> input)
+			{
+				ListNode pointer = this;
+				foreach (var element in input)
+				{
+					pointer.Value = element;
+					pointer.Next = new();
+					pointer = pointer.Next;
+				}
+			}
+
+			public void Print()
+			{
+				ListNode node = this;
+
+				while (node.Next != null)
+				{
+					Console.Write($"{node.Value,-3}");
+					node = node.Next;
+				}
+
+				Console.WriteLine();
+			}
 		}
 	}
 }
