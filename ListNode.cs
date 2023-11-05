2597860ecbc59ca159cb00a7e7d7738cd80d8c9e
Solveur <bsluch@gmail.com>
2023-11-05 12:21:56 +0200

    Добавьте файлы проекта.

---
+++ ListNode.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkedLists
+{
+	internal partial class Program
+	{
+		class ListNode
+		{
+			public ListNode Next { get; set; }
+			public int Value { get; set; }
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinkedLists
+{
+	internal partial class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+
+		void DeleteNode(ListNode node)
+		{
+			node.Next = node.Next.Next;
+			node.Value = node.Next.Value;
+		}
+	}
+}
